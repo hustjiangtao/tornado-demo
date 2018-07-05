d37592f45e5a11addfc33cfb74d82bb550c6dbdd
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-07-06 02:43:21 +0800

    Update: cached expire time & check if cached

--- handlers/post_list.py
+++ handlers/post_list.py
@@ -17,14 +17,16 @@ class PostListHandler(BaseHandler):
         offset = random.randint(0, 100)
         limit = 20
         all_posts = json_decode(self.r.get(name=self.request.uri))
+        cached = True if all_posts else False
         if not all_posts:
             all_posts = post_db.get_all_posts(offset=offset, limit=limit, search=search)
-            self.r.setex(name=self.request.uri, value=json_encode(all_posts), time=5)
+            self.r.setex(name=self.request.uri, value=json_encode(all_posts), time=60)
 
         data = {
             "offset": offset,
             "limit": limit,
             "search": search,
             "posts": all_posts,
+            "cached": cached,
         }
         self.render('post/post_list.html', data=data)
