aa3d1b1aa5f9f97cb3e0ad0ded7c0696b4d73352
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-08-13 14:01:29 +0800

    Add: 分页器

--- handlers/post_list.py
+++ handlers/post_list.py
@@ -16,8 +16,11 @@ class PostListHandler(BaseHandler):
 
     def get(self):
         search = self.get_query_argument('search', None)
-        offset = random.randint(0, 100)
+        # offset = self.get_query_argument('offset', None)
+        page = self.get_query_argument('page', 1)
+        # offset = random.randint(0, 100)
         limit = 20
+        offset = (int(page) - 1) * limit
         all_posts = json_decode(self.r.get(name=self.request.uri))
         cached = True if all_posts else False
         if not all_posts:
@@ -26,13 +29,14 @@ class PostListHandler(BaseHandler):
                 self.r.setex(name=self.request.uri, value=json_encode(all_posts), time=60)
 
         data = {
+            "page": page,
             "offset": offset,
             "limit": limit,
             "search": search,
             "posts": all_posts,
             "cached": cached,
         }
-        if self.request.uri == '/index':
+        if self.request.uri.startswith('/index'):
             self.render('mdl/index.html', data=data)
         else:
             self.render('post/post_list.html', data=data)
