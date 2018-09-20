eecb1048dc205baba4798ee293067e1cb05f3983
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-09-20 18:33:13 +0800

    Update: improve base api

--- urls.py
+++ urls.py
@@ -9,41 +9,46 @@ from tornado.web import RedirectHandler
 from handlers.test import TestHandler
 from handlers.health import HealthHandler
 from handlers.demo import DemoHandler
-from handlers.register import RegisterHandler
-from handlers.auth import AuthHandler
-from handlers.user import UserHandler
-from handlers.post_list import PostListHandler
-from handlers.post import PostHandler
-from handlers.chat import ChatIndexHandler
-from handlers.chat import ChatHandler
-from handlers.upload import UploadHandler
-from handlers.album import AlbumHandler
-from handlers.blog import BlogHandler
-from handlers.about import AboutHandler
-from handlers.contact import ContactHandler
+from handlers.auth import register
+from handlers.auth import auth
+from handlers.user import user
+from handlers.post import post_list
+from handlers.post import post
+from handlers.tools import chat
+from handlers.tools import upload
+from handlers.album import album
+from handlers.blog import blog
+from handlers.blog import about
+from handlers.blog import contact
 
 
-URL_HANDLERS = [
+URL_HANDLERS = []
+
+
+__BASE_URL_HANDLERS = [
     (r"/", RedirectHandler, dict(url='/posts', permanent=False)),
     (r"/test", TestHandler),
     (r"/health", HealthHandler),
     (r"/demo", DemoHandler),
-    (r"/register", RegisterHandler),
-    (r"/auth", AuthHandler),
-    (r"/user", UserHandler),
-    (r"/posts", PostListHandler),
-    (r"/post", PostHandler),
-    (r"/chat-index", ChatIndexHandler),
-    (r"/chat", ChatHandler),
-    (r"/upload", UploadHandler),
-    (r"/album", AlbumHandler),
+    (r"/register", register.RegisterHandler),
+    (r"/auth", auth.AuthHandler),
+    (r"/user", user.UserHandler),
+    (r"/posts", post_list.PostListHandler),
+    (r"/post", post.PostHandler),
+    (r"/chat-index", chat.ChatIndexHandler),
+    (r"/chat", chat.ChatHandler),
+    (r"/upload", upload.UploadHandler),
+    (r"/album", album.AlbumHandler),
 ]
 
-__MDL_URL_HANDLERS = [
-    (r"/index", PostListHandler),
-    (r"/blog", BlogHandler),
-    (r"/about", AboutHandler),
-    (r"/contact", ContactHandler),
+
+__MDL_BLOG_URL_HANDLERS = [
+    (r"/index", post_list.PostListHandler),
+    (r"/blog", blog.BlogHandler),
+    (r"/about", about.AboutHandler),
+    (r"/contact", contact.ContactHandler),
 ]
 
-URL_HANDLERS.extend(__MDL_URL_HANDLERS)
+
+URL_HANDLERS.extend(__BASE_URL_HANDLERS)
+URL_HANDLERS.extend(__MDL_BLOG_URL_HANDLERS)
