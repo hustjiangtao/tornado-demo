1fbe22425ceb7966a54b57f765370aaf4abd96cf
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-09-21 14:57:18 +0800

    Add: deploy config for nginx & supervisor

---
+++ deploy/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# deploy for tornado_demo include nginx conf & supervisor conf
+
+cp /home/jiangtao/tornado_demo/deploy/nginx/tornado_demo.conf /etc/nginx/conf.d/tornado_demo.conf
+cp /home/jiangtao/tornado_demo/deploy/supervisord/tornado_demo.ini /etc/supervisord.d/tornado_demo.ini
+nginx -s reload
+supervisorctl update
