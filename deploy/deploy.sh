a0964fb2f512d391aa8e69dc45d81ef7b29a0892
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-09-26 19:07:49 +0800

    Update: deploy config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,6 @@
 # deploy for tornado_demo include nginx conf & supervisor conf
 
 cp /home/jiangtao/tornado_demo/deploy/nginx/tornado_demo.conf /etc/nginx/conf.d/tornado_demo.conf
-cp /home/jiangtao/tornado_demo/deploy/supervisord/tornado_demo.ini /etc/supervisord.d/tornado_demo.ini
+cp /home/jiangtao/tornado_demo/deploy/supervisor/tornado_demo.ini /etc/supervisord.d/tornado_demo.ini
 nginx -s reload
 supervisorctl update
