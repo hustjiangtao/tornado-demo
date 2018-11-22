19b437d4a6cb35a25b6585b21565289e9dc6a0b1
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-11-22 11:53:07 +0800

    Add: supervisor job config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,6 @@
 
 cp /home/jiangtao/tornado-demo/deploy/nginx/tornado_demo.conf /etc/nginx/conf.d/tornado_demo.conf
 cp /home/jiangtao/tornado-demo/deploy/supervisor/tornado_demo.ini /etc/supervisord.d/tornado_demo.ini
+cp /home/jiangtao/tornado-demo/deploy/supervisor/job.ini /etc/supervisord.d/job.ini
 nginx -s reload
 supervisorctl update
